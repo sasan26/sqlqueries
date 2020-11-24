db63519057fb52460a6536bb681d310bbff508bb
sasan Bazade <sasan.bazadeh@gmail.com>
2020-11-23 16:39:48 -0800

    new

--- Sasan.java
+++ Sasan.java
@@ -11,7 +11,8 @@ public class Sasan {
     public static char[] passString = System.console().readPassword("%s", "Enter password: ");
     public static String Pass = new String(passString);
     public static String B = System.getProperty("line.separator");
-    public static String[] files = {"mailers.txt","pass.txt","tax.txt"};
+    public static String[] files = {"sasan.txt"};
+    public static String[] passwordSas = {"sasan"};
 
    public void output(boolean batchFind, String bid,String date,String p,String tray1, String tray2, String checknum, String sapimport, String st, String batch){        
         System.out.println("\t Id: \t\t" + bid);
@@ -26,13 +27,83 @@ public class Sasan {
             System.out.println("\t Batch: \t" + batch);
         }
    }
-                        
+
+   public void passpopup(){
+    JPanel panel = new JPanel();
+    JLabel label = new JLabel("Enter password:");
+    JPasswordField pass = new JPasswordField(10);
+    panel.add(label);
+    panel.add(pass);
+    String[] options = new String[]{"OK"};
+     JOptionPane.showOptionDialog(null, panel, "PASSWORD APP",
+                                  JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE,
+                                  null, options, options[0]);
+    
+        char[] password = pass.getPassword();
+        String Pass = new String(password);
+        passwordSas[0] = Pass;
+        // return Pass2;
+    }
+
+    public void changeDb(String userinput, String dbinput){
+        switch (userinput){
+            case "DB>MAILERS": files[0] = "mailers.txt"; System.out.println(B+"\t\t\tCurrent database => [ Mailers ]"); break;  // mailers.txt
+            case "DB>TAX": files[0] = "tax.txt"; System.out.println(B+"\t\t\tCurrent database => [ Tax ]"); break;  // tax.txt
+            default: passpopup(); if(passwordSas[0].equals("sasi")){files[0] = "pass.txt"; System.out.println(B+"\t\t\tCurrent database => [ Password ]");} else{System.out.println("\t\t\tWrong Password!");}  // pass.txt
+            }  
+    }
+
+                           
     public void vName(String vname){
-        
+
+        if (vname.equals("HELP")){
+            System.out.print( B );
+            for(int i =0; i<46; i++){System.out.print( "-" );}
+            System.out.print(" List of commands ");
+            for(int i =0; i<46; i++){System.out.print( "-" );}
+            System.out.println( B );
+            System.out.println("\t[ help ]\t\t\t\t\tfor information");
+            System.out.println("\t[ all ]\t\t\t\t\t\tfor whole mailer IDs list");
+            System.out.println("\t[ number ]\t\t\t\t\tsearch by number \t\t ex: 26");
+            System.out.println("\t[ date ]\t\t\t\t\tsearch by sent Date \t\t ex: date>09/04/2020");
+            System.out.println("\t[ check ] + [ >check number ]\t\t\tsearch by check number \t\t ex: check>6145");
+            System.out.println("\t[ id ] + [ >ID number ]\t\t\t\tsearch by ID \t\t\t ex: id>MO-NJ_1518_2");
+            System.out.println("\t[ state ] + [ >state abbreviation ]\t\tsearch by State \t\t ex: state>tx");
+            System.out.println("\t[ batch ] + [ >Batch number ] \t\t\tfor all data with same batch \t ex: batch>2");
+            System.out.println("\t[ batch ] + [ >Batch number ] + [ >status ]\tshipment status(batch#) \t ex: batch>2>status");
+            System.out.println("\t[ batch ] + [ >Batch number ] + [ >sap ]\tSAP status(batch#) \t\t ex: batch>2>sap");
+            System.out.println("\t[ exit ]\t\t\t\t\tquit from app"+B);
+            for(int i =0; i<110; i++){System.out.print( "-" );} 
+            System.out.print( B );
+        }		
+           
+
+        if(vname.equals("DB>PASSWORD")){ changeDb( vname, "DB>PASSWORD"); }
+        if(files[0].equals("pass.txt")){
+            try {
+                File myObj3 = new File(files[0]);
+                Scanner myReader3 = new Scanner(myObj3);
+                
+                
+                while (myReader3.hasNextLine()) {
+                    String db = myReader3.nextLine();
+                    files[0] = db;
+                    System.out.println(db);
+                }
+                myReader3.close();
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+
+        if(vname.equals("DB>MAILERS")){ changeDb( vname, "DB>MAILERS"); }
+        if(files[0].equals("mailers.txt")){ 
             try {
                 File myObj = new File(files[0]);
                 Scanner myReader = new Scanner(myObj);
-               
+                
                 
                 while (myReader.hasNextLine()) {
                     String data = myReader.nextLine();
@@ -85,11 +156,11 @@ public class Sasan {
                             case "shipped": System.out.println("\t\t| SHIPPED | => " + batchId ); break;
                             case "hold": System.out.println("\t\t| HOLD    | => " + batchId ); break;
                             default: System.out.println("\t\t| " + status + " | => " + batchId );
-                          }                        
+                            }                        
                     }
                     if(vname.equals("BATCH>"+batch+">SAP")){      
                         if (sap.equals("+")){      
-                          System.out.println("\t\t [+] ADDED => "+batchId);
+                            System.out.println("\t\t [+] ADDED => "+batchId);
                         } else {System.out.println("\t\t [ ]       => " + batchId);}
                     }  
 
@@ -101,27 +172,52 @@ public class Sasan {
                             System.out.println("\t =============");
                             output(false, batchId, sent, pieces, t1, t2, check, sap, status, batch);  
                         } 
-                      }
-
-                    
+                    } 
                 }
                 System.out.println(B);
                 myReader.close();
             } catch (FileNotFoundException e) {
                 System.out.println("An error occurred.");
                 e.printStackTrace();
-              }
+            }
+        }
         
     }
 
+    public void creditCrator(){
+        System.out.print(B);
+        for(int i =0; i<110; i++){System.out.print( "*" );}
+        printCredit("Sasan APP", true);
+        printCredit("version 2.1.0", false);
+        printCredit("by Sasan Bazade", false);	 
+        for(int i =0; i<110; i++){System.out.print( "*" );}
+        System.out.print(B);
+      }
+      public void printCredit(String text, boolean brline){
+        if(!brline){
+          System.out.print("\t\t\t\t\t\t" + text + B );
+        } else {
+          System.out.print(B + "\t\t\t\t\t\t" + text + B );
+        }    
+      }
+    
+      public void checkPassword(){
+        System.out.print("Password: ");
+        for(int i=0; i< Pass.length(); i++){ 
+          System.out.print("*");
+        } 
+        System.out.print(B);
+      }
 
     public static void main(String[] args) {
           
         Sasan mailersData = new Sasan();
         Scanner in = new Scanner(System.in);    
+        mailersData.checkPassword();
+        mailersData.creditCrator(); 
             
         if(Pass.equals("sasan")){ 
-            // checkPasswordName.printCredit("#type help  for more information", false);  
+            mailersData.printCredit("> type help  for more information", false);  
             while ( Pass.equals("sasan") ) {
                 try {
                 String name = in.next(); 
@@ -135,7 +231,7 @@ public class Sasan {
                 }
             }    
         } 
-        // else { checkPasswordName.printCredit("WRONG PASSWORD!", true); }
+        else { mailersData.printCredit("WRONG PASSWORD!", true); }
         in.close();         
     }
 
